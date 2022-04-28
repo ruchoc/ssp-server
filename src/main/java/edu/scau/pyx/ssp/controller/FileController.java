34730d46c568d005a0f2eb80a3d237686d600b19
xuan765 <765551361@qq.com>
2022-04-28 14:11:41 +0800

    4.28

--- src/main/java/edu/scau/pyx/ssp/controller/FileController.java
+++ src/main/java/edu/scau/pyx/ssp/controller/FileController.java
@@ -1,5 +1,7 @@
 package edu.scau.pyx.ssp.controller;
 
+import edu.scau.pyx.ssp.service.FileService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,11 @@ import java.io.File;
 @RestController
 @RequestMapping(value = "/file")
 public class FileController {
-    @RequestMapping(value = "/uoloadpic")
-    public booleam upload(@RequestParam(value = "pic")MultipartFile multipartFile, @RequestParam(value = "shareId")long shareId){
+    @Autowired
+    private FileService fileService;
 
+    @RequestMapping(value = "/uoloadpic")
+    public boolean upload(@RequestParam(value = "pic")MultipartFile multipartFile, @RequestParam(value = "shareId")long shareId){
+        return fileService.uploadPicture(multipartFile, shareId);
     }
 }
