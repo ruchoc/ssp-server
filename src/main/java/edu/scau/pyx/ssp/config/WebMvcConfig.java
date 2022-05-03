0e40205669bc590bee702f224c260bc77e2733b4
xuan765 <765551361@qq.com>
2022-05-03 22:36:10 +0800

    5.3 update

---
+++ src/main/java/edu/scau/pyx/ssp/config/WebMvcConfig.java
@@ -0,0 +1,16 @@
+package edu.scau.pyx.ssp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")   //所有方法都做处理跨域
+                .allowedOrigins("http://localhost:80")  //允许跨域的请求头
+                .allowedMethods("*")  //允许通过地请求方法
+                .allowedHeaders("*");  //允许的请求头
+    }
+}
