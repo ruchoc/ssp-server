fdbe83bf5fc6d10b3773abaad3b84d4a25140f56
xuan765 <765551361@qq.com>
2022-05-07 14:44:33 +0800

    update 5.7

--- src/main/java/edu/scau/pyx/ssp/config/WebMvcConfig.java
+++ src/main/java/edu/scau/pyx/ssp/config/WebMvcConfig.java
@@ -9,10 +9,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")   //所有方法都做处理跨域
+                .allowedHeaders("*")  //允许的请求头
+                .allowedMethods("*")  //允许通过地请求方法
                 //.allowedOrigins("*")  //允许跨域的请求头
                 .allowedOriginPatterns("*")//允许跨域的请求头
-                .allowedMethods("*")  //允许通过地请求方法
-                .allowedHeaders("*")  //允许的请求头
                 .allowCredentials(true);//允许携带cookie参数
     }
 }
