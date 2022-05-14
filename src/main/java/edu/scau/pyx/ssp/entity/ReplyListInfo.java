5f7e40c2fa67f66b016dc0b865cbd797c6e6e6e0
xuan765 <765551361@qq.com>
2022-05-15 00:27:43 +0800

    update 5.14

--- src/main/java/edu/scau/pyx/ssp/entity/ReplyListInfo.java
+++ src/main/java/edu/scau/pyx/ssp/entity/ReplyListInfo.java
@@ -6,20 +6,24 @@ public class ReplyListInfo {
     private long id;
     private long sendUserId;
     private String sendUserName;
+    private String sendUserAvatarUrl;
     private long acceptUserId;
     private String acceptUserName;
+    private String acceptUserAvatarUrl;
     private String content;
     private Date time;
 
     public ReplyListInfo() {
     }
 
-    public ReplyListInfo(long id, long sendUserId, String sendUserName, long acceptUserId, String acceptUserName, String content, Date time) {
+    public ReplyListInfo(long id, long sendUserId, String sendUserName, String sendUserAvatarUrl, long acceptUserId, String acceptUserName, String acceptUserAvatarUrl, String content, Date time) {
         this.id = id;
         this.sendUserId = sendUserId;
         this.sendUserName = sendUserName;
+        this.sendUserAvatarUrl = sendUserAvatarUrl;
         this.acceptUserId = acceptUserId;
         this.acceptUserName = acceptUserName;
+        this.acceptUserAvatarUrl = acceptUserAvatarUrl;
         this.content = content;
         this.time = time;
     }
@@ -48,6 +52,14 @@ public class ReplyListInfo {
         this.sendUserName = sendUserName;
     }
 
+    public String getSendUserAvatarUrl() {
+        return sendUserAvatarUrl;
+    }
+
+    public void setSendUserAvatarUrl(String sendUserAvatarUrl) {
+        this.sendUserAvatarUrl = sendUserAvatarUrl;
+    }
+
     public long getAcceptUserId() {
         return acceptUserId;
     }
@@ -62,6 +74,14 @@ public class ReplyListInfo {
 
     public void setAcceptUserName(String acceptUserName) {
         this.acceptUserName = acceptUserName;
+    }
+
+    public String getAcceptUserAvatarUrl() {
+        return acceptUserAvatarUrl;
+    }
+
+    public void setAcceptUserAvatarUrl(String acceptUserAvatarUrl) {
+        this.acceptUserAvatarUrl = acceptUserAvatarUrl;
     }
 
     public String getContent() {
